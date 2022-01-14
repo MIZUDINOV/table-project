c29019df4caa75e8b6134c31a042bb25615a5a1a
Mukhamed <izudinov.work@gmail.com>
2022-01-14 18:31:42 +0300

    app component update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EffectsModule } from '@ngrx/effects';
 
 import { AppComponent } from './app.component';
 import { TableModule } from './components/table/table.module';
+import { TableEffects } from './store/effects/table/table.effects';
+import { CustomStoreModule } from './store/store.module';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, TableModule],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        TableModule,
+        CustomStoreModule,
+        EffectsModule.forRoot([TableEffects]),
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
