bfcfbd544654f63083efcd1c3f4f149e85e9f3fc
Mukhamed <izudinov.work@gmail.com>
2022-01-14 18:35:39 +0300

    update table component

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,85 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-
-export interface PeriodicElement {
-    id: number;
-    name: string;
-    username: string;
-    email: string;
-    phone: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-    {
-        id: 1,
-        name: 'Hydrogen',
-        username: 'afadfsd',
-        email: 'some@mail.ru',
-        phone: '2452345',
-    },
-    {
-        id: 2,
-        name: 'Helium',
-        username: 'afadfsd',
-        email: 'some@mail.ru',
-        phone: '456456',
-    },
-    {
-        id: 3,
-        name: 'Lithium',
-        username: 'afadfsd',
-        email: 'some@mail.ru',
-        phone: '453433333',
-    },
-    {
-        id: 4,
-        name: 'Beryllium',
-        username: 'afadfsd',
-        email: 'some@mail.ru',
-        phone: '7686786',
-    },
-    {
-        id: 5,
-        name: 'Boron',
-        username: 'afadfsd',
-        email: 'some@mail.ru',
-        phone: '34535',
-    },
-    {
-        id: 6,
-        name: 'Carbon',
-        username: 'afadfsd',
-        email: 'some@mail.ru',
-        phone: '6675675',
-    },
-    {
-        id: 7,
-        name: 'Nitrogen',
-        username: 'afadfsd',
-        email: 'some@mail.ru',
-        phone: '533456',
-    },
-    {
-        id: 8,
-        name: 'Oxygen',
-        username: 'afadfsd',
-        email: 'some@mail.ru',
-        phone: '54636453',
-    },
-    {
-        id: 9,
-        name: 'Fluorine',
-        username: 'afadfsd',
-        email: 'some@mail.ru',
-        phone: '2243534567777',
-    },
-    {
-        id: 10,
-        name: 'Neon',
-        username: 'afadfsd',
-        email: 'some@mail.ru',
-        phone: '90-340989345',
-    },
-];
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { tableActions } from 'src/app/store/actions';
+import { tableSelectors } from 'src/app/store/selectors';
+import { User } from 'src/app/store/state/table';
+import { ModalComponent } from '../modal/modal.component';
 
 @Component({
     selector: 'table-comp',
@@ -87,9 +13,33 @@ const ELEMENT_DATA: PeriodicElement[] = [
     styleUrls: ['./table.style.scss'],
 })
 export class TableComponent implements OnInit {
-    dataSource = ELEMENT_DATA;
-    displayedColumns: string[] = ['id', 'name', 'username', 'email', 'phone'];
+    public readonly dataSource$: Observable<User[]> = this.store.select(
+        tableSelectors.getSelectUsers
+    );
+    public readonly displayedColumns: string[] = [
+        'id',
+        'name',
+        'username',
+        'email',
+        'phone',
+    ];
+
+    constructor(
+        private store: Store<{ users: User[] }>,
+        private readonly dialog: MatDialog
+    ) {}
+
     ngOnInit(): void {
-        //dispatch action
+        this.store.dispatch(tableActions.fetchUsers());
+    }
+
+    changeTableUserData(data: User) {
+        const dialogConfig = new MatDialogConfig();
+        dialogConfig.disableClose = true;
+        dialogConfig.autoFocus = true;
+        dialogConfig.width = '30%';
+        dialogConfig.data = data;
+
+        this.dialog.open(ModalComponent, dialogConfig);
     }
 }
--- src/app/components/table/table.module.ts
+++ src/app/components/table/table.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
+
 import { MatTableModule } from '@angular/material/table';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
 
 import { TableComponent } from './table.component';
+import { ModalComponent } from '../modal/modal.component';
+import { EditUserDataModule } from '../modal/modal.module';
 
 @NgModule({
+    imports: [
+        MatTableModule,
+        MatDialogModule,
+        MatIconModule,
+        EditUserDataModule,
+    ],
     declarations: [TableComponent],
-    imports: [MatTableModule],
+    entryComponents: [ModalComponent],
     exports: [TableComponent],
 })
 export class TableModule {}
